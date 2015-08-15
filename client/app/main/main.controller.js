cf4bd11b3a44a9df97fcac90b225865dce1fe475
Omri Litov <omrilitov@gmail.com>
2015-08-15 21:45:10 +0300

    Taskbar partially working

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,33 +1,31 @@
 'use strict';
 
 angular.module('taskbarApp')
-  .controller('Main', function ($scope, $interval) {
+  .controller('Main', function ($scope, $interval, $timeout) {
     //var _ = require('lodash');
     //var path = require('path');
-    //var Q = require('q');
-    //var nachosApi = require('nachos-api');
-    //var client = nachosApi.server;
+    var Q = require('q');
+    var nachosApi = require('nachos-api');
+    var client = nachosApi.server;
     //var remote = require('remote');
     var windowz = remote.require('windowz');
 
     //var batteryLevel = require('battery-level');
     //var brightness = require('brightness');
-    //var vol = require('vol');
+    var vol = require('vol');
 
     $scope.date = Date.now();
     $interval(function () {
       $scope.date = Date.now();
     }, 1000);
 
-    //$scope.grid = grid;
-
     //$scope.windows = _.groupBy(windows.getAll(), function (window) {
     //  //return window.process.name;
     //});
 
     $scope.windows = windowz.getAll();
-
-    /*var checkBattery = function () {
+/*
+    var checkBattery = function () {
       Q.nfcall(batteryLevel)
         .then(function (batteryLevel) {
           $scope.batteryLevel = batteryLevel;
@@ -66,7 +64,7 @@ angular.module('taskbarApp')
       var calc = ((parseInt($scope.brightness / jumps) + 1) % (1 / jumps + 1)) * jumps;
       brightness.set(calc);
     };
-
+*/
     var checkVolume = function () {
       Q.nfcall(vol.get)
         .then(function (volume) {
@@ -88,7 +86,7 @@ angular.module('taskbarApp')
       var calc = ((parseInt($scope.volume / jumps) + 1) % (1 / jumps + 1)) * jumps;
       vol.set(calc);
     };
-
+/*
     workspaces.onWorkspacesChanged(function () {
       return updateWorkspaceMeta();
     });
@@ -100,13 +98,13 @@ angular.module('taskbarApp')
         });
     }
 
-    updateWorkspaceMeta();
+    updateWorkspaceMeta();*/
 
     client.users.me()
       .then(function (user) {
         $scope.user = user;
       });
-
+/*
     $scope.getNumberOfWindowsClass = function (window) {
       if (window.length > 9) {
         return 'mdi-numeric-9-plus-box-multiple-outline';
@@ -117,11 +115,21 @@ angular.module('taskbarApp')
       else {
         return 'mdi-numeric-' + window.length + '-box-multiple-outline'
       }
-    };
-
+    };*/
+/*
     $scope.changeWorkspace = function (id) {
       workspaces.changeWorkspace(id);
-    };*/
+    };
+*/
+    nachosApi.on('shell:editModeChanged', function (isEdit) {
+      $timeout(function () {
+        $scope.isEditMode = isEdit;
+      });
+    });
+
+    $scope.toggleEditMode = function () {
+      nachosApi.emit('shell:toggleEditMode');
+    };
 
     $scope.windowClick = function (window) {
       windowz.activate(window.handle);
